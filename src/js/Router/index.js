74dc7f0167bac164edc2314dc00096fd32b16526
Dennis Ego <dennis.ego@artcoresociety.com>
2024-12-19 09:26:02 +0100

    Project structure setup

---
+++ src/js/Router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/Pages/Home.vue';
+
+const routes = [
+	{
+		path: '/',
+		name: 'home_page',
+		component: Home
+	},
+	{
+		path: '/:catchAll(.*)',
+		redirect: '/'
+	}
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes
+});
+
+export default router;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './styles/app.scss';
+import router from '@/Router';
+import App from '@/Layouts/AppLayout.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router).mount('#app');
